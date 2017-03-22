932ee2dc8bb6c2ebd2a89b1647a453e6595633a6
Lorenzo Mele <lorenzo@loman.it>
2017-03-23 00:43:43 +0100

    added stop call

--- pychatbot/bot.py
+++ pychatbot/bot.py
@@ -1,118 +1,118 @@
 import json
 
+
 class Bot(object):
-	commands = []
-	command_prepend = "/"
-	endpoints = []
-	
-	def default_response(self, in_message):
-		pass
-    
-	def process(self, in_message):
-		if in_message.startswith(self.command_prepend) and in_message[1:] in self.commands:
-			f = self.__getattribute__(in_message[1:])
-			return f(self)
-		return self.default_response(in_message)
+    commands = []
+    command_prepend = "/"
+    endpoints = []
 
+    def default_response(self, in_message):
+        pass
 
-	def http_serve(self):
-		from http.server import HTTPServer, BaseHTTPRequestHandler
-		import http
-		from threading import Thread
-		try:
-			from urllib.parse import parse_qs
-		except ImportError:
-			from urlparse import parse_qs
+    def process(self, in_message):
+        if in_message.startswith(self.command_prepend) and in_message[1:] in self.commands:
+            f = self.__getattribute__(in_message[1:])
+            return f(self)
+        return self.default_response(in_message)
 
-		PORT = 8000
+    def http_serve(self):
+        from http.server import HTTPServer, BaseHTTPRequestHandler
+        import http
+        from threading import Thread
+        try:
+            from urllib.parse import parse_qs
+        except ImportError:
+            from urlparse import parse_qs
 
-		class Handler(BaseHTTPRequestHandler):
-			def do_GET(s):
-				
-				function, params = s.path.split("?")
-				function, params = function[1:], parse_qs(params)
-				
-				s.send_response(200)
-				s.end_headers()
-				output = {
-					"out_message": self.process("".join(params["in_message"]))
-				}
-				s.wfile.write(json.dumps(output).encode("UTF-8"))
-				
-		def serve_loop():
-			while self.http_on:
-				self.httpd.handle_request()
+        PORT = 8000
 
-		self.httpd = HTTPServer(('', PORT), Handler)
-		self.http_on = True
-		self.http_thread = Thread(target=serve_loop)
-		self.http_thread.daemon = True
-		self.http_thread.start()
-		
-		while not self.http_thread.is_alive():
-			pass
-		
+        class Handler(BaseHTTPRequestHandler):
+            def do_GET(s):
+                function, params = s.path.split("?")
+                function, params = function[1:], parse_qs(params)
 
-	def http_stop(self):
-		self.http_on = False
+                s.send_response(200)
+                s.end_headers()
+                output = {
+                    "out_message": self.process("".join(params["in_message"]))
+                }
+                s.wfile.write(json.dumps(output).encode("UTF-8"))
 
-		while self.http_thread.is_alive():
-			self.httpd.server_close()
-			
+        def serve_loop():
+            while self.http_on:
+                self.httpd.handle_request()
 
-	def telegram_serve(self,token):
-		from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-		
-		self.telegram = Updater(token)
-		
-		# commands
+        self.httpd = HTTPServer(('', PORT), Handler)
+        self.http_on = True
+        self.http_thread = Thread(target=serve_loop)
+        self.http_thread.daemon = True
+        self.http_thread.start()
 
-		def telegram_command_handler(bot, update):
-			command = update.message.text[1:]
-			f = self.__getattribute__(command)
-			update.message.reply_text(f(self))
-		self.telegram_command_handler = telegram_command_handler
-		
-		for command in self.commands:
-			self.telegram.dispatcher.add_handler(
-				CommandHandler(
-					command,
-					self.telegram_command_handler
-				)
-			)
-		
-		# default message handler
-		
-		def telegram_message_handler(bot, update):
-			message = update.message.text
-			update.message.reply_text(self.default_response(message))
-		self.telegram_message_handler = telegram_message_handler
-			
-		self.telegram.dispatcher.add_handler(
-			MessageHandler(
-				Filters.text,
-				self.telegram_message_handler
-			)
-		)
-		
-		self.telegram.start_polling()
+        while not self.http_thread.is_alive():
+            pass
 
+    def http_stop(self):
+        self.http_on = False
 
-	def add_endpoint(self,endpoint):
-		self.endpoints.append(endpoint)
+        while self.http_thread.is_alive():
+            self.httpd.server_close()
 
+    def telegram_serve(self, token):
+        from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-	def start(self):
-		for ep in self.endpoints:
-			ep.start()
+        self.telegram = Updater(token)
+
+        # commands
+
+        def telegram_command_handler(bot, update):
+            command = update.message.text[1:]
+            f = self.__getattribute__(command)
+            update.message.reply_text(f(self))
+
+        self.telegram_command_handler = telegram_command_handler
+
+        for command in self.commands:
+            self.telegram.dispatcher.add_handler(
+                CommandHandler(
+                    command,
+                    self.telegram_command_handler
+                )
+            )
+
+        # default message handler
+
+        def telegram_message_handler(bot, update):
+            message = update.message.text
+            update.message.reply_text(self.default_response(message))
+
+        self.telegram_message_handler = telegram_message_handler
+
+        self.telegram.dispatcher.add_handler(
+            MessageHandler(
+                Filters.text,
+                self.telegram_message_handler
+            )
+        )
+
+        self.telegram.start_polling()
+
+    def add_endpoint(self, endpoint):
+        self.endpoints.append(endpoint)
+
+    def start(self):
+        for ep in self.endpoints:
+            ep.start()
+
+    def stop(self):
+        for ep in self.endpoints:
+            ep.stop()
 
 
 # decorator    
 class command():
+    def __init__(self, f):
+        Bot.commands.append(f.__name__)
+        self.f = f
 
-	def __init__(self, f):
-		Bot.commands.append(f.__name__)
-		self.f = f
-
-	def __call__(self, *args, **kwargs):
-		return self.f(*args, **kwargs)
+    def __call__(self, *args, **kwargs):
+        return self.f(*args, **kwargs)
--- tests/base.py
+++ tests/base.py
@@ -1,47 +1,51 @@
 from pychatbot.bot import Bot
 
+
 def test_create_bot_object():
     b = Bot()
     assert isinstance(b, Bot)
 
     
 def test_default_behaviour():
-	class MyBot(Bot):
-		def default_response(self, in_message):
-			return in_message
-			
-	b = MyBot()
-	assert b.process("hello") == "hello"
-	assert b.process("123") == "123"
-	assert b.process("/hello") == "/hello"
-	
+    class MyBot(Bot):
+        def default_response(self, in_message):
+            return in_message
+
+    b = MyBot()
+    assert b.process("hello") == "hello"
+    assert b.process("123") == "123"
+    assert b.process("/hello") == "/hello"
+    
 
 def test_add_command():
-	from pychatbot.bot import command
-	
-	class MyBot(Bot):
-		@command
-		def hello(self):
-			return "hello!"
-			
-		@command
-		def bye(self):
-			return "goodbye..."
-		
-	b = MyBot()
-	assert b.process("/hello") == "hello!"
-	assert b.process("/bye") == "goodbye..."
+    from pychatbot.bot import command
+
+    class MyBot(Bot):
+        @command
+        def hello(self):
+            return "hello!"
+
+        @command
+        def bye(self):
+            return "goodbye..."
+
+    b = MyBot()
+    assert b.process("/hello") == "hello!"
+    assert b.process("/bye") == "goodbye..."
 
 
-def test_add_endpoint(mocker):
-	class MyBot(Bot):
-		pass
-	
-	endpoint = mocker.Mock()
-	
-	bot = MyBot()
-	bot.add_endpoint(endpoint)
-	bot.start()
-	
-	assert endpoint.start.called
-	
+def test_add_endpoint_start_stop(mocker):
+    class MyBot(Bot):
+        pass
+
+    endpoint = mocker.Mock()
+
+    bot = MyBot()
+    bot.add_endpoint(endpoint)
+    bot.start()
+
+    assert endpoint.start.called
+
+    bot.stop()
+
+    assert endpoint.stop.called
