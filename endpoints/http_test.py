d8a4d37001058cf1a5ea7f871d44279f1ad21439
Lorenzo Mele <lorenzo@loman.it>
2017-03-24 07:22:21 +0100

    moved http endpoint test alongside the http endpoint definition

--- endpoints/http_test.py
+++ endpoints/http_test.py
@@ -1,23 +1,24 @@
+from __future__ import absolute_import
 from http.client import HTTPConnection
 import json
-from threading import Thread
-from time import sleep
 try:
     from urllib.parse import urlencode
 except ImportError:
     from urllib import urlencode
 
-from pychatbot.bot import Bot, command
+from pychatbot.bot import Bot
 from endpoints.http import HttpEndpoint
 
 
-def test_http_interface():
+def test_http_interface_new():
     class MyBot(Bot):
         def default_response(self, in_message):
             return in_message[::-1]
 
     bot = MyBot()
-    bot.http_serve()
+    ep = HttpEndpoint()
+    bot.add_endpoint(ep)
+    bot.start()
 
     test_messages = ["hello", "another message"]
     for tm in test_messages:
@@ -29,27 +30,4 @@ def test_http_interface():
         assert ret["out_message"] == tm[::-1]
         conn.close()
 
-    bot.http_stop()
-
-
-def test_http_command():
-    class MyBot(Bot):
-        def default_response(self, in_message):
-            return in_message[::-1]
-
-        @command
-        def start(self):
-            return "Welcome!"
-
-    b = MyBot()
-    b.http_serve()
-
-    conn = HTTPConnection("127.0.0.1:8000")
-    conn.request("GET", "/process?in_message=/start")
-    r = conn.getresponse()
-    assert r.status == 200
-    ret = json.loads(r.read().decode())
-    assert ret["out_message"] == "Welcome!"
-    conn.close()
-
-    b.http_stop()
+    bot.stop()
