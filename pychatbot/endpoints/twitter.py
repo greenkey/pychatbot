54ead1fa7e5a475ef338fbdf65fe4243ef5e7fb4
Lorenzo Mele <greenkey@loman.it>
2017-04-16 00:45:57 +0200

    moved to another twitter library, code is a mess

--- pychatbot/endpoints/twitter.py
+++ pychatbot/endpoints/twitter.py
@@ -5,22 +5,59 @@
 """
 
 from __future__ import absolute_import
-import tweepy
+from threading import Thread
+from time import sleep
+
+import twitter
 
 
 class TwitterEndpoint(object):
 
     def __init__(self, consumer_key, consumer_secret,
                  access_token, access_token_secret):
-        self._auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        self._auth.set_access_token(access_token, access_token_secret)
-        self._api = tweepy.API(self._auth)
+        self._api = twitter.Api(consumer_key=consumer_key,
+                                consumer_secret=consumer_secret,
+                                access_token_key=access_token,
+                                access_token_secret=access_token_secret)
+        self._polling_thread = Thread(target=self.polling_new_direct_messages)
+        self.calculate_last_processed_dm()
+        self._polling_is_running = False
 
     def set_bot(self, bot):
         self._bot = bot
 
     def run(self):
-        pass
+        self._pollint_running = False
+        self._polling_thread.start()
+
+        while not self._polling_is_running:
+            sleep(0.5)
 
     def stop(self):
-        pass
+        self._polling_should_run = False
+
+    def polling_new_direct_messages(self):
+        self._polling_should_run = True
+
+        while self._polling_should_run:
+            dms = self._api.GetDirectMessages(
+                since_id=self._last_processed_dm
+            )
+            for direct_message in dms:
+                self.process_new_direct_message(direct_message)
+            self._polling_is_running = True
+            sleep(5)
+
+    def process_new_direct_message(self, direct_message):
+        response = self._bot.process(in_message=direct_message.text)
+
+        self._api.PostDirectMessage(response, user_id=direct_message.sender.id)
+        self._last_processed_dm = direct_message.id
+
+    def calculate_last_processed_dm(self):
+        self._last_processed_dm = 0
+        for direct_message in self._api.GetDirectMessages():
+            self._last_processed_dm = max(
+                self._last_processed_dm,
+                direct_message.id
+            )
--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,16 +1,37 @@
 """ Unit tests for pychatbot.endpoints.TelegramEndpoint
 """
 
+import json
+from time import time, sleep
+from twitter.models import DirectMessage
+
 from pychatbot.bot import Bot
 from pychatbot.endpoints import TwitterEndpoint
+
+
+def create_message(text):
+    return DirectMessage().NewFromJsonDict(json.loads(
+        '{"id": 0, "text": "%s", '
+        '"sender": {"id": 0, "screen_name": "greenkey", "name": "greenkey"},'
+        '"sender_screen_name": "greenkey"}' % text))
+
+
+def wait_for(check_callback, polling_time=0.1, timeout=5):
+    start = time()
+    while time() - start < timeout:
+        if check_callback():
+            break
+        sleep(polling_time)
+    else:
+        assert False
+    assert True
 
 
 def test_twitter_interface(mocker):
     """ Test that the Twitter API is called when using the endpoint.
     """
 
-    tweepyOAuth = mocker.patch('tweepy.OAuthHandler')
-    tweepyAPI = mocker.patch('tweepy.API')
+    twitterAPI = mocker.patch('twitter.Api')
 
     class MyBot(Bot):
         "Lowering bot"
@@ -34,9 +55,60 @@ def test_twitter_interface(mocker):
     bot.add_endpoint(endpoint)
     bot.run()
 
-    tweepyOAuth.assert_called_once_with(consumer_key, consumer_secret)
-    tweepyOAuth().set_access_token.assert_called_once_with(
-        access_token, access_token_secret)
-    tweepyAPI.assert_called_with(tweepyOAuth())
+    twitterAPI.assert_called_once_with(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token_key=access_token,
+        access_token_secret=access_token_secret
+    )
+
+    bot.stop()
+
+
+def test_twitter_default_response(mocker):
+    """ Test that the Twitter bot correctly reply with the default response.
+    """
+
+    twitterAPI = mocker.patch('twitter.Api')
+
+    class MyBot(Bot):
+        "Upp&Down bot"
+
+        last_in_message = ''
+
+        def default_response(self, in_message):
+            self.last_in_message = in_message
+            return ''.join([
+                c.upper() if i % 2 else c.lower()
+                for i, c in enumerate(in_message)
+            ])
+
+    bot = MyBot()
+    endpoint = TwitterEndpoint(
+        consumer_key='consumer_key',
+        consumer_secret='consumer_secret',
+        access_token='access_token',
+        access_token_secret='access_token_secret'
+    )
+    bot.add_endpoint(endpoint)
+    bot.run()
+
+    message = create_message("SuperCamelCase")
+    twitterAPI().GetDirectMessages.return_value = [message]
+
+    wait_for(lambda: bot.last_in_message == "SuperCamelCase")
+
+    twitterAPI().PostDirectMessage.assert_called_with(
+        'sUpErCaMeLcAsE', user_id=message.sender.id
+    )
+
+    message = create_message("plain boring text")
+    twitterAPI().GetDirectMessages.return_value = [message]
+
+    wait_for(lambda: bot.last_in_message == "plain boring text")
+
+    twitterAPI().PostDirectMessage.assert_called_with(
+        'pLaIn bOrInG TeXt', user_id=message.sender.id
+    )
 
     bot.stop()
